73d3de4f2cba55332f4bd0208b500476c0e26bc4
David <david_why@outlook.com>
2021-02-12 13:42:40 +0800

    feat: Change code for question permutations

--- 2020.02.12.permutations.cpp
+++ 2020.02.12.permutations.cpp
@@ -6,22 +6,34 @@ using namespace std;
 int INITIAL;
 bool used[MAXN + 1] = {false};
 int nums[MAXN + 1] = {0};
+int ans = 0;
 
 void permutations(int n)
 {
-    for (int i = 1; i <= n; i++)
+    if (n == 0)
+    {
+        for (int i = 0; i < INITIAL; i++)
+            cout << nums[i] << " ";
+        cout << "\n";
+        ans++;
+        return;
+    }
+    for (int i = 1; i <= INITIAL; i++)
     {
         if (used[i])
             continue;
         nums[INITIAL - n] = i;
         used[i] = true;
         permutations(n - 1);
+        used[i] = false;
     }
 }
 
 int main()
 {
     cin >> INITIAL;
-    permutations(INITIAL);
+    if (INITIAL != 0)
+        permutations(INITIAL);
+    cout << ans << endl;
     return 0;
 }
