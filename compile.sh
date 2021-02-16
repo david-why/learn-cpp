53bb5449594176cb653d6a5adbebf2f5681061c8
David <david_why@outlook.com>
2021-02-16 13:08:50 +0800

    feat: Unset unused variable in compile.sh

--- compile.sh
+++ compile.sh
@@ -2,6 +2,7 @@
 g++ "$1" -o "$1.out" -static-libstdc++ -static-libgcc -O3 -Wall
 export ret="$?"
 if [ "$ret" != "0" ]; then exit $ret; fi
+unset ret
 x86_64-w64-mingw32-g++ "$1" -o "$1.exe" -static-libstdc++ -static-libgcc -O3 --no-warnings
 "./$1.out"
 exit $?
