43cce824e87aa43bbc036e473d7806c428d83ff6
David <david_why@outlook.com>
2021-04-11 20:02:24 +0800

    feat: Add COMPILE OK output in compile.sh

--- compile.sh
+++ compile.sh
@@ -4,5 +4,6 @@ export ret="$?"
 if [ "$ret" != "0" ]; then exit $ret; fi
 unset ret
 x86_64-w64-mingw32-g++ "$1" -o "$1.exe" -static-libstdc++ -static-libgcc -O3 --no-warnings
+echo 'COMPILE OK, running'
 "./$1.out"
 exit $?
