6147232c2e9a6cf1b1d659cc9a74b077d093b7d3
David <david_why@outlook.com>
2021-05-10 20:43:00 +0800

    feat: Add quiet mode to compile.sh

--- compile.sh
+++ compile.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+out=true
+if [ "$1" == "--quiet" ] || [ "$1" == "-q" ]; then
+    out=false
+    shift
+fi
 g++ "$1" -o "$1.out" -static-libstdc++ -static-libgcc -O3 -Wall
-export ret="$?"
+ret="$?"
 if [ "$ret" != "0" ]; then exit $ret; fi
 unset ret
 x86_64-w64-mingw32-g++ "$1" -o "$1.exe" -static-libstdc++ -static-libgcc -O3 --no-warnings
-echo 'COMPILE OK, running'
+$out && echo 'COMPILE OK, running'
 "./$1.out"
 exit $?
